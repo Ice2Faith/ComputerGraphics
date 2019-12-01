28736049debbad2ab0c2531569f1be9805aadbd4
Ice2Faith <45895088+Ice2Faith@users.noreply.github.com>
2019-12-01 17:39:44 +0800

    upload source code vc6 item

---
+++ ComputerGraphics/GGrating.h
@@ -0,0 +1,20 @@
+#ifndef _GGRATING_H_
+#define _GGRATING_H_
+#include"GData.h"
+#include<Windows.h>
+class GGrating
+{
+public:
+	static void BezierLine(HDC hdc, GD2Point points[], int pointCount,COLORREF color);
+	static void DDALine(HDC hdc, GD2Point p1, GD2Point p2, COLORREF color);
+	static void AntialiasLine(HDC hdc, GD2Point p1, GD2Point p2, COLORREF fgc, COLORREF bgc);
+	static void SeedFillAlgorithm(HDC hdc, GD2Point seed, COLORREF bgc, COLORREF flc);
+	static void TrangleFillScanLine(GD2Flat flat, COLORREF fillcolor, HDC hdc);
+	//创建Bezier旋转体绕Y轴
+	static GD3DataGroup* CreateBezierSpinCube(GD2Point ctrlPoints[],int ctrlPointCount,int tCount=30,int rAngleCount=50);
+private:
+	static double Bernstein(int i, int n, double t);
+	static double BezierX(int n, double t, GD2Point points[]);
+	static double BezierY(int n, double t, GD2Point points[]);
+};
+#endif //_GGRATING_H_
