b34b46a276e31e8f805073f454bf176b5edc4c98
Ice2Faith <45895088+Ice2Faith@users.noreply.github.com>
2019-12-02 15:56:52 +0800

    Update GStereo.h

--- ComputerGraphics/GStereo.h
+++ ComputerGraphics/GStereo.h
@@ -4,23 +4,23 @@
 class GStereo
 {
 public:
-	//ËÄÃæÌå
+	//å››é¢ä½“
     static GD3DataGroup* CreateTetrahedron(double a=100);
-	//ÁùÃæÌå
+	//å…­é¢ä½“
     static GD3DataGroup* CreateHexahedron(double a=100);
-	//°ËÃæÌå
+	//å…­é¢ä½“
     static GD3DataGroup* CreateOctahedron(double a=100);
-	//Ê®¶şÃæÌå
+	//åäºŒé¢ä½“
 	static GD3DataGroup* CreateDodecahedron(double a=100);
-	//¶şÊ®ÃæÌå
+	//äºŒåé¢ä½“
     static GD3DataGroup* CreateIcosahedron(double a=100);
-	//ÇòÌå
+	//çƒä½“
     static GD3DataGroup* CreateBall(double r=300,int aAngleCount=300,int bAngleCount=300);
-	//Ô²ÖùÌå
+	//åœ†æŸ±ä½“
     static GD3DataGroup* CreateCylinder(double r=150,double h=200,int rCount=20,int hCount=300,int rAngleCount=300);
-	//Ô²×¶Ìå
+	//åœ†é”¥ä½“
     static GD3DataGroup* CreateCone(double h=400,double r=200,int hCount=200,int rCount=200,int rAngleCount=300);
-	//»·Ìå
+	//ç¯ä½“
     static GD3DataGroup* CreateTorus(double r1=200,double r2=50,int aAngleCount=200,int bAngleCount=100);
 };
 #endif // _GSTEREO_H_
